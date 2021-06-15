0280d07ca89b66474672d44131c3ea1717cab972
Joaquín Torrens <71098233+TorrensJoaquin@users.noreply.github.com>
2021-06-14 21:53:46 -0300

    Update 01_Index.js

--- 01_Index.js
+++ 01_Index.js
@@ -135,7 +135,7 @@ function CreateInputsIn00(){
     aux=aux+30;
 }
 function mousePressed(){
-    if (event.type != 'touchstart') return true;
+    if (event.type != 'mousedown') return true;
     webButtons[0].checkIfThisClickIsForMe();
     webButtons[1].checkIfThisClickIsForMe();
     webButtons[2].checkIfThisClickIsForMe();
